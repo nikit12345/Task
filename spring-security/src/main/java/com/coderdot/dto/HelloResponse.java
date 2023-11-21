c947685a1da8b2a09591d608f6e1c667e1292854
nikita <nikitamane2000.com>
2023-11-21 21:02:47 +0530

    commit

---
+++ spring-security/src/main/java/com/coderdot/dto/HelloResponse.java
@@ -0,0 +1,12 @@
+package com.coderdot.dto;
+
+public class HelloResponse {
+	String message;
+
+	public HelloResponse(String message) {
+		super();
+		this.message = message;
+	}
+	
+
+}
---
+++ spring-security/src/main/java/com/coderdot/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.coderdot.dto;
+
+public class LoginResponse {
+	
+	String jwt;
+
+	public LoginResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+	
+
+}
